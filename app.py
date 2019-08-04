b9e7d9c58a2a403f6ec0eb93ac1ef583a2a9a9a8
grandprixgp <akala@porkpi.com>
2019-08-04 09:24:16 +0100

    have you ever retired a human by mistake

--- app.py
+++ app.py
@@ -3,32 +3,46 @@ from flask import Flask, escape, request
 
 from ctypes import *
 
-lib = cdll.LoadLibrary("quickdemo/build/release/module/quickdemo-windows-4.0-amd64.dll")
-#lib.Dump.argtypes = [c_char_p]
-#print(lib.Dump("yes"))
+class GoString(Structure):
+    _fields_ = [("p", c_char_p), ("n", c_longlong)]
+
+go_library = cdll.LoadLibrary("quickdemo/build/release/quickdemo-windows-4.0-amd64.dll")
+go_library.Dump.argtypes = [GoString]
+go_library.Dump.restype = c_char_p
 
 util = importlib.import_module("util")
 app = Flask(__name__)
 
+def dump(demo_files):
+    go_arg = GoString(" ".join(demo_files).encode(), sum(map(len, demo_files)) + 1)
+    go_return = go_library.Dump(go_arg)
+    return go_return.decode()
+
 @app.route('/live')
 def live():
-    demo_files = util.find_extension("dem", hours = 2)
-    return "<br>".join(demo_files)
+    return app.response_class(
+        response=json.dumps(dump(util.find_extension("dem", hours = 2))),
+        status=200,
+        mimetype='application/json'
+    )
 
 @app.route('/recent')
 def recent():
-    demo_files = util.find_extension("dem", hours = 4)
+    demo_files = dump(util.find_extension("dem", hours = 4))
     return "<br>".join(demo_files)
 
 @app.route('/today')
 def today():
-    demo_files = util.find_extension("dem", days = 1)
+    demo_files = dump(util.find_extension("dem", days = 1))
     return "<br>".join(demo_files)
 
 @app.route('/week')
 def week():
-    demo_files = util.find_extension("dem", weeks = 1)
-    return "<br>".join(demo_files)
+    return app.response_class(
+        response=json.dumps(dump(util.find_extension("dem", weeks = 1))),
+        status=200,
+        mimetype='application/json'
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
